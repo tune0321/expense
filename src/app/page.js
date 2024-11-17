b0e3be81f04b2eb9417b2ed89d243bbbba0b4a96
tnaoki0630 <naoki.tsunezawa.s8@dc.tohoku.ac.jp>
2024-11-17 19:39:10 +0900

    大工事

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import ExpenseTracker from '../components/ExpenseTracker';
-
-export default function Home() {
-  return (
-    <main className="container mx-auto py-8 px-4">
-      <ExpenseTracker />
-    </main>
-  );
-}
