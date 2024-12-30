ac0f171ce8371b03a7dbc19deb4bd6a9f4f98543
tune0321 <tune9126@gmail.com>
2024-12-30 17:34:53 +0900

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,4 @@
 import ExpenseTracker from '../components/ExpenseTracker';
-import './globals.css'; #お試し
 export default function Home() {
   return (
     <main className="container mx-auto py-8 px-4">
